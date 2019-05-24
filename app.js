5458303c65963b6592ca3975eb5513d75feee686
Lubo <skycube@gmail.com>
2019-05-24 15:51:02 +0300

    Simple Login Completed

--- app.js
+++ app.js
@@ -11,45 +11,52 @@ app.use(morgan('short'));
 app.use(express.static("./public"));
 app.use(router);
 
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'Health-2019',
+    database: 'healthdb'
+});
+
+
+function sendHTML(fileName, res){
+  let options = { root: path.join(__dirname, '/public') }
+  console.log(options.root,fileName);
+  res.sendFile(fileName, options, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Sent:', fileName);
+    }
+  });
+}
+
 app.use("/", (req, res) => {
   console.log(">>>>>>>>>>>>>>>",req.body.userName, req.body.passWord);
-
   if(!req.body.userName){
-      let fileName = "loader.html";
-      let options = { root: path.join(__dirname, '/public') }
-      console.log(options.root,fileName);
-      res.sendFile(fileName, options, (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('Sent:', fileName);
-        }
-      });
+    sendHTML("loader.html", res);
 
   } else if (req.body.userName == "-"){
-      let fileName = "login.html";
-      let options = { root: path.join(__dirname, '/public') }
-      console.log(options.root,fileName);
-      res.sendFile(fileName, options, (err) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log('Sent:', fileName);
-        }
-      });
+    sendHTML("login.html", res);
 
     } else {
-      let fileName = "start.html";
-      let options = { root: path.join(__dirname, '/public') }
-      console.log(options.root,fileName);
-      res.sendFile(fileName, options, (err) => {
+      const queryString = "SELECT * FROM healthdb.users WHERE userName=? AND passWord=? LIMIT 1"
+      connection.query(queryString, [req.body.userName, req.body.passWord], (err,rows,fields) => {
+        console.log("checking user/pass combination");
+        console.log(rows.length, rows);
         if (err) {
-          console.log(err);
+            console.log(err);
+            return false;
+        } else if (rows.length > 0){
+          sendHTML("start.html", res);
         } else {
-          console.log('Sent:', fileName);
+          sendHTML("login.html", res);
         }
-      });
-    }
+    });
+  }
 });
 app.use(/*default redirect when not found*/(req, res) => {
     res.status(404).send("Not found!");
